10ef3a671d9623ba241b8af106ff04ec5b275bf5
tovwvy <104623411+tovwvy@users.noreply.github.com>
2024-02-28 11:10:40 +0200

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,9 @@
+var_int = 10
+var_float = 8.4
+
+# Розділення var_int на var_float
+print("var_int / var_float:", var_int / var_float)
+
+# Розділення big_int на var_float
+big_int = 35.0
+print("big_int / var_float:", big_int / var_float)
