cfce3fc3aebe39d4d197047665d3ae0820828541
tovwvy <104623411+tovwvy@users.noreply.github.com>
2024-03-13 14:17:05 +0200

    Update 8.py

--- 8.py
+++ 8.py
@@ -1,16 +1,30 @@
-# Зчитування чисел a, b, c з клавіатури
-a = int(input("Введіть число a: "))
-b = int(input("Введіть число b: "))
-c = int(input("Введіть число c: "))
+def read_integers():
+    
+  a = int(input("Введіть число a: "))
+  b = int(input("Введіть число b: "))
+  c = int(input("Введіть число c: "))
+  return a, b, c
 
-# Підрахунок кількості додатних чисел
-positive_count = 0
-if a > 0:
-  positive_count += 1
-if b > 0:
-  positive_count += 1
-if c > 0:
-  positive_count += 1
+def count_positives(a, b, c):
+  """
+  Функція підраховує кількість додатних чисел.
+  """
+  positive_count = 0
+  if a > 0:
+    positive_count += 1
+  if b > 0:
+    positive_count += 1
+  if c > 0:
+    positive_count += 1
+  return positive_count
 
-# Вивід результату
-print("Кількість додатних чисел:", positive_count)
+def print_results(positive_count):
+  """
+  Функція виводить результат.
+  """
+  print("Кількість додатних чисел:", positive_count)
+
+# Виклик функцій
+a, b, c = read_integers()
+positive_count = count_positives(a, b, c)
+print_results(positive_count)
