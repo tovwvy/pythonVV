894cf72048928bf54503dda516af50b08ca1b9c8
tovwvy <104623411+tovwvy@users.noreply.github.com>
2024-03-13 14:16:57 +0200

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,16 +1,30 @@
-# Зчитування чисел a, b, c з клавіатури
-a = int(input("Введіть число a: "))
-b = int(input("Введіть число b: "))
-c = int(input("Введіть число c: "))
+def read_integers():
+   
+  a = int(input("Введіть число a: "))
+  b = int(input("Введіть число b: "))
+  c = int(input("Введіть число c: "))
+  return a, b, c
 
-# Підрахунок кількості негативних чисел
-negative_count = 0
-if a < 0:
-  negative_count += 1
-if b < 0:
-  negative_count += 1
-if c < 0:
-  negative_count += 1
+def count_negatives(a, b, c):
+  """
+  Функція підраховує кількість негативних чисел.
+  """
+  negative_count = 0
+  if a < 0:
+    negative_count += 1
+  if b < 0:
+    negative_count += 1
+  if c < 0:
+    negative_count += 1
+  return negative_count
 
-# Вивід результату
-print("Кількість негативних чисел:", negative_count)
+def print_results(negative_count):
+  """
+  Функція виводить результат.
+  """
+  print("Кількість негативних чисел:", negative_count)
+
+# Виклик функцій
+a, b, c = read_integers()
+negative_count = count_negatives(a, b, c)
+print_results(negative_count)
